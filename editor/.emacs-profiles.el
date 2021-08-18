95bfd09e5d1c80d67ea3996f46fd11329dce063e
toyboot4e <toyboot4e@gmail.com>
2021-08-18 18:40:55 +0900

    add WIP installation commands

---
+++ editor/.emacs-profiles.el
@@ -0,0 +1,7 @@
+(
+    ("default"  .  ((user-emacs-directory . "~/dotfiles/editor/emacs-evil")))
+    ("evil"     .  ((user-emacs-directory . "~/dotfiles/editor/emacs-evil")))
+    ("vanilla"  .  ((user-emacs-directory . "~/dotfiles/editor/emacs-vanilla")))
+    ("doom"     .  ((user-emacs-directory . "~/fixed/doom-emacs")))
+    ("spc"      .  ((user-emacs-directory . "~/fixed/spacemacs")))
+)
--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -2,10 +2,6 @@
 
 status --is-interactive; and source ~/dotfiles/shell/fish/interactive.fish
 
-if command -sq rvm
-    rvm default
-end
-
 # tabtab source for electron-forge package
 # uninstall by removing these lines or running `tabtab uninstall electron-forge`
 [ -f /Users/toy/repos/xi-electron/node_modules/tabtab/.completions/electron-forge.fish ]; and . /Users/toy/repos/xi-electron/node_modules/tabtab/.completions/electron-forge.fish
