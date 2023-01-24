5a974a1f85aa5c634d0ed4029d8541861c9c7863
toyboot4e <toyboot4e@gmail.com>
2023-01-25 07:52:42 +0900

    nixos: update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,55 +1,51 @@
 # configuration.nix(5)
 # nixos-help
 
-{ config, pkgs, callPackage, ... }: {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+{ config, pkgs, ... }:
 
-  # TODO: Hide user preferences?
-  time.timeZone = "Asia/Tokyo";
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
 
-  # Allow most packages TODO: What does it mean?
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+      # home-manager as NixOS module:
+      # <https://nix-community.github.io/home-manager/index.html#sec-install-nixos-module>
+      <home-manager/nixos>
+    ];
+
+  nixpkgs.config.allowUnfree = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable sudo TODO: Is it OK?
-  security.sudo.enable = true;
-  security.sudo.configFile = ''
-    %wheel ALL=(ALL) ALL
-  '';
-
-  # TODO: Network settings
   # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;
-  networking.networkmanager.enable = true;
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp4s0.useDHCP = true;
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # TODO: Select keyboard layout here
   # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+  console = {
+    # font = "Lat2-Terminus16";
+    # keyMap = "us";
+    # useXkbConfig = true; # use xkbOptions in tty.
+    keyMap = "jp106";
+    earlySetup = true;
+    packages = with pkgs; [ terminus_font ];
+    font = "ter-u14n";
+  };
 
-  # Who don't want sound?
+  # Enable sound.
   sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   # Fonts https://nixos.wiki/wiki/Fonts
   fonts = {
@@ -75,6 +71,43 @@
     };
   };
 
+  # X11 + i3
+  # https://nixos.wiki/wiki/I3
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
+  services.xserver = {
+    enable = true;
+
+    desktopManager = { };
+    displayManager.defaultSession = "none+i3";
+
+    videoDrivers = [ "nvidia" ];
+
+    # bye, capslock
+    xkbOptions = "ctrl:nocaps";
+
+    windowManager.i3 = {
+      enable = true;
+
+      extraPackages = with pkgs; [
+        dmenu
+        # TODO: Rofi here?
+        # rofi
+        i3status
+        i3lock
+        i3blocks #if you are planning on using i3blocks over i3status
+      ];
+    };
+  };
+
+  # TODO: nedded?
+  hardware.opengl.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
   # Enable Japanese input
   #
   # https://ykonomi.hatenablog.com/entry/2021/04/27/022803
@@ -90,139 +123,50 @@
     fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
   };
 
-  # Set up `xfce` + `i3` (I failed to setup `i3` only environment; I even failed to enable Wifi)
-  # https://nixos.wiki/wiki/I3
-  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
-  services.xserver = {
-    # Use the X window system
-    enable = true;
-
-    desktopManager = {
-      # a. Gnome (can't be used alongside `i3`)
-      # gnome.enable = true;
-
-      # # b. `xfce` + WM
-      # xfce = {
-      #   enable = true;
-      #   noDesktop = true;
-      #   enableXfwm = false;
-      # };
-    };
-
-    displayManager = {
-      # a. Gnome: enable the diplsay manager
-      # gdm.enable = true;
-
-      # b. `xfce`
-      # defaultSession = "xfce+i3";
-
-      # c. `i3` only
-      defaultSession = "none+i3";
-    };
-    
-    windowManager = {
-      i3 = {
-        enable = true;
-
-	# use `i3-gaps` just to change `i3bar` height
-        # package = pkgs.i3-gaps; 
-
-        extraPackages = with pkgs; [
-	  # TODO: Rofi?
-          dmenu
-          i3status
-          i3lock
-          i3blocks #if you are planning on using i3blocks over i3status
-       ];
-      };
-    };
-  };
-
-  # -----
-  # Set up HomeManager (per-user package management in declarative style)
-  # https://nixos.wiki/wiki/Home_Manager
-  # -----
-
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # System
-    rxvt_unicode
-    python3
-    python2
-
-    # System GUI/utilities
-    rofi
-    flameshot
-
-    # GUI
-    qutebrowser firefox chromium
-    discord
-    slack
-    vscode
-    mpv
-    gimp
-
-    # Terminal
-    vimHugeX   # system Vim with clipboard support
-    kitty alacritty
-    bash fish
-    tmux
-    git gh ghq
-    emacs neovim nodejs
-    fish
-
-    # CLI Tools
-    ffmpeg imagemagick
-    # bandcamp-dl
-    youtube-dl
-    ripgrep fd bat delta exa as-tree
-    tokei du-dust
-    zoxide ranger
-    tealdeer
-    w3m
-
-    # Default
-    wget
-    zip unzip
-    # unrar
-    xclip
-
-    # Development
-    cmake ninja llvm
-    gcc go rustup
+    vimHugeX xclip wget curl
+    kitty bash fish zsh tmux git gh ghq w3m fzf
+    ripgrep fd bat delta exa as-tree tokei zoxide ranger tealdeer
+    direnv nix-direnv stack cabal-install
+    qutebrowser firefox ffmpeg imagemagick dmenu rofi flameshot xdragon
   ];
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # shell
-  environment.binsh = "${pkgs.dash}/bin/dash";
-
-  # this is only for root?
-  users = {
-    defaultUserShell = pkgs.fish;
+  # Steam: https://nixos.wiki/wiki/Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
-  # Set password with `passwd`
+  # home-manager as a NixOS module: https://nix-community.github.io/home-manager/index.html#sec-install-nixos-module
+
   users.users.tbm = {
     shell = pkgs.fish;
     isNormalUser = true;
-    # wheek for `sudo`, `networkManager` for network stetings in non-Gnome env (`i3`)
-    extraGroups = [ "wheel" "networkManager" ];
+    extraGroups = [ "wheel" "audio" "networkManager" ];
   };
 
-  environment.variables = {
-    PATH = "$HOME/.cargo/bin:$HOME/.local/bin:$HOME/go/bin:$HOME/config/bin:$HOME/bin";
+  # home-manager as NixOS module: <https://nix-community.github.io/home-manager/index.html>
+  home-manager.users.tbm = { pkgs, ... }: {
+    nixpkgs.config.allowUnfree = true;
+
+    home.packages = with pkgs; [
+      emacs neovim
+      rustup go python3 nodejs
+      discord slack vscode mpv gimp
+    ];
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "22.11";
   };
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -236,7 +180,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  programs.ssh.startAgent = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    # permitRootLogin = "yes";
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -244,13 +194,18 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 
 }
 
