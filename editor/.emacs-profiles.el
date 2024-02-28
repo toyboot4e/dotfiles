0a98df86670510aca76f8eb5e75db1088c06fd06
toyboot4e <toyboot4e@gmail.com>
2024-02-28 23:45:03 +0900

    wip

--- editor/.emacs-profiles.el
+++
@@ -1,6 +0,0 @@
-(
-    ("default"     .  ((user-emacs-directory . "~/dotfiles/editor/emacs-leaf")))
-    ("leaf"     .  ((user-emacs-directory . "~/dotfiles/editor/emacs-leaf")))
-    ("doom"     .  ((user-emacs-directory . "~/fixed/doom-emacs")))
-    ("spc"      .  ((user-emacs-directory . "~/fixed/spacemacs")))
-)
