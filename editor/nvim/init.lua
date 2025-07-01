a790040c8dfaedced6b4de952ed8e5ef3b58c0d5
toyboot4e <toyboot4e@gmail.com>
2025-07-01 21:12:34 +0900

    wip

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -340,6 +340,44 @@ require("lazy").setup({
 },
 
 {
+  "Josiah-tan/plover-vim",
+  config = function()
+    --
+  end
+},
+
+{
+  "Josiah-tan/plover-vim-tutor",
+  dependencies = { 'Josiah-tan/plover-vim' },
+  config = function()
+    vim.keymap.set('n', '<leader>ap', '<cmd>help plover-vim-tutor<CR>') -- open tape window
+  end
+},
+
+{
+  "derekthecool/plover-tapey-tape.nvim",
+  dependencies = { 'derekthecool/plover-tapey-tape.nvim', 'derekthecool/plover-tapey-tape.nvim' },
+  config = function()
+    vim.keymap.set('n', '<leader>at', require('plover-tapey-tape').toggle) -- open tape window
+    vim.keymap.set('n', '<leader>as', require('plover-tapey-tape').stop) -- stop plugin
+    require('plover-tapey-tape').setup({
+      -- filepath = 'auto',
+      -- open_method = 'vsplit',
+      -- vertical_split_height = 9,
+      -- horizontal_split_width = 54,
+      -- steno_capture = '|(.-)|',
+      -- suggestion_notifications = {
+      --     enabled = true,
+      -- },
+      -- status_line_setup = {
+      --     enabled = true,
+      --     additional_filter = '(|.-|)',
+      -- },
+    })
+  end
+},
+
+{
   -- 'hukl/Smyck-Color-Scheme',
   'dim13/smyck.vim',
   -- lazy = false,
