102562a68eaf25734740f7d184d8a50dc21cec53
toyboot4e <toyboot4e@gmail.com>
2026-04-21 07:37:06 +0900

    wip

--- nix/ssh-keys.nix
+++ nix/ssh-keys.nix
@@ -2,6 +2,6 @@
   # SSH public keys for each machine.
   tbm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDD/kIFriN1rT6rTuGVJWS6bg77PfyNnIG6OL7Sopqtk toyboot4e@gmail.com";
   mac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDD/kIFriN1rT6rTuGVJWS6bg77PfyNnIG6OL7Sopqtk toyboot4e@gmail.com";
-  mp = "ssh-ed25519 XXXX mp@mp";
+  mp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFEossDEbiYxqeBEXrFzEOn2HW0/F1TFSFL5be1BrNY your_email@example.com";
   win = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDD/kIFriN1rT6rTuGVJWS6bg77PfyNnIG6OL7Sopqtk toyboot4e@gmail.com";
 }
